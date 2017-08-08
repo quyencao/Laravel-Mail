8af7614c7c39db751e7549cc35ec494a2d15641a
quyencao <boy20195@gmail.com>
2017-08-09 00:18:05 +0700

    Set up event/listener

--- resources/views/emails/users/newuserwelcome.blade.php
+++ resources/views/emails/users/newuserwelcome.blade.php
@@ -1,21 +1,14 @@
 @component('mail::message')
-# Welcome to my laravel app
+# Welcome to {{ $user->name }}
 
 Thank for **signing up**. Let us know how we can do more to _please_ you!
 
+@component('mail:panel')
+    The email address you signed up with us: {{ $user->email }}
+@endcomponent
+
 @component('mail::button', ['url' => 'http://localhost:8000/home', 'color' => 'green'])
 Let use our service now
-@endcomponent
-
-@component('mail::panel')
-    This is the panel content.
-@endcomponent
-
-@component('mail::table')
-    | Laravel       | Table         | Example  |
-    | ------------- |:-------------:| --------:|
-    | Col 2 is      | Centered      | $10      |
-    | Col 3 is      | Right-Aligned | $20      |
 @endcomponent
 
 Thanks,<br>
